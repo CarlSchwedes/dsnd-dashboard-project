6c2bdd2ae024c630444b304b512b4ed4436cdc82
CarlSchwedes <carl.schwedes@gmail.com>
2026-07-28 15:37:43 +0200

    init project setup, sql db scripts, python analysis scripts

---
+++ python-package/employee_events/__init__.py
@@ -0,0 +1,6 @@
+from .employee import Employee
+from .team import Team
+from .query_base import QueryBase
+from .sql_execution import *
+
+__all__ = ['Employee', 'Team', 'QueryBase', 'sql_execution']
